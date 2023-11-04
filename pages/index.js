c9250e461a07a4069610b93862a03c21bf5743c7
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2023-11-04 17:40:16 +0800

    added animation

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,14 @@ export default function Home() {
       <div className="flex flex-col text-7xl hover:text-[84px] transition-all duration-500 items-center justify-center font-bold z-20">
         <span className="">Шинэчлэгдсэн</span>
         <span className="text-[#aaf082] ">Ардкойн 3.0</span>
-        <span className="mt-5 text-2xl font-normal rounded-xl border-white/10 p-3 bg-white/5">
-          Decentralized
-        </span>
+        <div className="flex items-center mt-5 text-2xl font-normal rounded-lg border-[#223e42] border-[1px] px-3 py-2 bg-white/5">
+          <div id="text" className="mr-2">
+            Centralized
+          </div>
+          <div id="text">
+            <span>to Decentralized</span>
+          </div>
+        </div>
       </div>
     </main>
   );
