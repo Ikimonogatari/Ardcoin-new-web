8ddbe6b6ab9da158d02f6171d4de4d8d72b0fe76
Ikimonogatari <tuvshuutuvshinjargal21@gmail.com>
2023-12-04 17:00:57 +0800

    updated

--- pages/index.js
+++ pages/index.js
@@ -127,7 +127,7 @@ export default function Home() {
     };
   }, []);
   return (
-    <main className="w-full h-screen relative flex justify-center items-center">
+    <main className="w-full h-screen relative flex justify-center items-center overflow-hidden">
       <img
         className="absolute h-full w-full z-0 hidden sm:block"
         src="/bg.webp"
@@ -153,7 +153,7 @@ export default function Home() {
         loading="lazy"
         className="absolute bottom-0 w-full z-50 opacity-70 h-1/6 mx-auto block md:hidden object-cover transition-opacity duration-1000 ease-in-out"
       />
-      <div className="flex flex-col transition-all duration-500 items-center justify-center font-bold z-20 max-w-6xl max-h-[750px] container overflow-hidden">
+      <div className="flex flex-col transition-all duration-500 items-center justify-center font-bold z-20 container">
         {/* <span className="text-[#aaf082] ">Ardcoin 3.0</span> */}
 
         <img
